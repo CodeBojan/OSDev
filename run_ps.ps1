1a7f9dc9029c5fd3aaf8cccd1ad4ba1f97dc9568
bojanramic3000@gmail.com <bojanramic3000@gmail.com>
2024-09-24 15:21:15 +0200

    Modified load.asm and other files to enable reading beyond the 512 bytes of the first sector.

--- run_ps.ps1
+++ run_ps.ps1
@@ -1,12 +1,12 @@
-$bootloader = "boot1.bin"
+$bootloader_name = "boot1"
 $disk_image = "floppy.img"
 $raised = $false
 
 qemu-img create -f raw floppy.img 1440K
-nasm -f bin -o boot1.bin boot1.asm
+nasm -f bin -o "$($bootloader_name).bin" "$($bootloader_name).asm"
 
 try{
-$bootloader_data = [System.IO.File]::ReadAllBytes($bootloader)
+$bootloader_data = [System.IO.File]::ReadAllBytes($bootloader_name + ".bin")
 
 $disk_image_file = [System.IO.File]::OpenWrite($disk_image)
 $disk_image_file.Write($bootloader_data, 0, $bootloader_data.Length)
@@ -21,6 +21,7 @@ catch{
 if(!$raised){
 	Write-Host "Bootloader written onto disk image."
 	qemu-system-x86_64 -drive format=raw,file=floppy.img
+	Remove-Item -Path ".\$($bootloader_name).bin"
 }
 
 
